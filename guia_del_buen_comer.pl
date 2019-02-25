2dac6aa8284a7545b25073cd867ec0da9675c02a
walorey <facu-a77aque@hotmail.com.ar>
2019-02-25 01:19:46 -0300

    hasta el 3

--- guia_del_buen_comer.pl
+++ guia_del_buen_comer.pl
@@ -1,6 +1,32 @@
+/*1 de que producto proviene cada alimento*/
+
+deQueProviene(A,X):-
+	alimento(A,X).
+/*2 derivados */
+
+esLacteo(A):-
+	derivaDe(A,leche).
+
+tieneGluten(A):-
+	derivaDe(A,trigo).
+
+derivaDeAnimal(A):-
+	derivaDe(A,cerdo).
+
+derivaDeAnimal(A):-
+	derivaDe(A,vaca).
+
+derivaDe(A,X):-
+	alimento(A,Y),
+	derivaDe(Y,X).
+
+derivaDe(A,X):-
+	alimento(A,X).
+
+
 /* 3 modelar alimentos */
-(pan,trigo).
-(jamon,cerdo).
-(leche,vaca).
-(queso,leche).
-(arroz,-).
+alimento(pan,trigo).
+alimento(jamon,cerdo).
+alimento(leche,vaca).
+alimento(queso,leche).
+alimento(arroz,-).
